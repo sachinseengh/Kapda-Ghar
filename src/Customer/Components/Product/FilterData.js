8a52070736cacb24941070405ed4234a14519d3c
Sachin kumar Singh <108042077+sachinseengh@users.noreply.github.com>
2024-09-19 18:38:28 +0545

    filter section

--- src/Customer/Components/Product/FilterData.js
+++ src/Customer/Components/Product/FilterData.js
@@ -38,11 +38,11 @@ export const singleFilter=[
     id: "price",
     name: "Price",
     options:[
-    { value: "159-399", label: "*159 To *399" },
-    { value: "399-999", label: "*399 To *999" },
-    { value: "999-1999", label: "999 To 1999" },
-    { value: "1999-2999", label: "*1999 To *2999" },
-    { value: "3999-4999", label: "*3999 To *4999" },
+    { value: "159-399", label: "Rs 159 To Rs 399" },
+    { value: "399-999", label: "Rs 399 To Rs 999" },
+    { value: "999-1999", label: "Rs 999 ToRs 1999" },
+    { value: "1999-2999", label: "Rs 1999 To Rs 2999" },
+    { value: "3999-4999", label: "Rs 3999 To Rs 4999" },
     ]
 },
 {
--- src/Customer/Components/Product/Product.jsx
+++ src/Customer/Components/Product/Product.jsx
@@ -1,17 +1,3 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
 'use client'
 
 import { useState } from 'react'
@@ -28,69 +14,30 @@ import {
   MenuItems,
 } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
+import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon } from '@heroicons/react/20/solid'
 import { mens_kurta } from '../../../Data/men_kurta'
 import ProductCard from './ProductCard'
 import { filters, singleFilter } from './FilterData'
 
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+
 const sortOptions = [
-  // { name: 'Most Popular', href: '#', current: true },
-  // { name: 'Best Rating', href: '#', current: false },
-  // { name: 'Newest', href: '#', current: false },
   { name: 'Price: Low to High', href: '#', current: false },
   { name: 'Price: High to Low', href: '#', current: false },
 ]
-// const subCategories = [
-//   { name: 'Totes', href: '#' },
-//   { name: 'Backpacks', href: '#' },
-//   { name: 'Travel Bags', href: '#' },
-//   { name: 'Hip Bags', href: '#' },
-//   { name: 'Laptop Sleeves', href: '#' },
-// ]
-// const filters = [
-//   {
-//     id: 'color',
-//     name: 'Color',
-//     options: [
-//       { value: 'white', label: 'White', checked: false },
-//       { value: 'beige', label: 'Beige', checked: false },
-//       { value: 'blue', label: 'Blue', checked: true },
-//       { value: 'brown', label: 'Brown', checked: false },
-//       { value: 'green', label: 'Green', checked: false },
-//       { value: 'purple', label: 'Purple', checked: false },
-//     ],
-//   },
-//   {
-//     id: 'category',
-//     name: 'Category',
-//     options: [
-//       { value: 'new-arrivals', label: 'New Arrivals', checked: false },
-//       { value: 'sale', label: 'Sale', checked: false },
-//       { value: 'travel', label: 'Travel', checked: true },
-//       { value: 'organization', label: 'Organization', checked: false },
-//       { value: 'accessories', label: 'Accessories', checked: false },
-//     ],
-//   },
-//   {
-//     id: 'size',
-//     name: 'Size',
-//     options: [
-//       { value: '2l', label: '2L', checked: false },
-//       { value: '6l', label: '6L', checked: false },
-//       { value: '12l', label: '12L', checked: false },
-//       { value: '18l', label: '18L', checked: false },
-//       { value: '20l', label: '20L', checked: false },
-//       { value: '40l', label: '40L', checked: true },
-//     ],
-//   },
-// ]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
 export default function Product() {
-  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
+  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
+
+
+
+
+
+  
 
   return (
     <div className="bg-white">
@@ -137,11 +84,11 @@ export default function Product() {
                         {section.options.map((option, optionIdx) => (
                           <div key={option.value} className="flex items-center">
                             <input
-                              defaultValue={option.value}
+                              value={option.value}
                               defaultChecked={option.checked}
                               id={`filter-mobile-${section.id}-${optionIdx}`}
-                              name={`${section.id}[]`}
-                              type="checkbox"
+                              name={section.id} // Ensure the same name for radio buttons in the same group
+                              type="checkbox" // Change checkbox to radio
                               className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                             />
                             <label htmlFor={`filter-mobile-${section.id}-${optionIdx}`} className="ml-3 min-w-0 flex-1 text-gray-500">
@@ -153,6 +100,7 @@ export default function Product() {
                     </DisclosurePanel>
                   </Disclosure>
                 ))}
+                
                 {singleFilter.map((section) => (
                   <Disclosure key={section.id} as="div" className="border-b border-gray-200 px-4 py-6">
                     <h3 className="-my-3 flow-root">
@@ -169,11 +117,11 @@ export default function Product() {
                         {section.options.map((option, optionIdx) => (
                           <div key={option.value} className="flex items-center">
                             <input
-                              defaultValue={option.value}
+                              value={option.value}
                               defaultChecked={option.checked}
                               id={`filter-${section.id}-${optionIdx}`}
-                              name={`${section.id}[]`}
-                              type="checkbox"
+                              name={section.id} // Ensure the same name for radio buttons in the same group
+                              type="radio" // Change checkbox to radio
                               className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                             />
                             <label htmlFor={`filter-${section.id}-${optionIdx}`} className="ml-3 text-sm text-gray-600">
@@ -241,11 +189,18 @@ export default function Product() {
 
           <section aria-labelledby="products-heading" className="pb-24 pt-6">
             <h2 id="products-heading" className="sr-only">Products</h2>
+           
+            <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5 ">
 
-            <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
               {/* Desktop Filters */}
-              <form className="hidden lg:block">
-              {filters.map((section) => (
+              <form className="hidden lg:block ">
+                
+            <div className='flex justify-between'>
+            <div className='font-semibold  opacity-55 text-2xl '>Filters</div>
+            <div className='opacity-55'><FilterAltIcon/></div>
+            </div>
+              
+                {filters.map((section) => (
                   <Disclosure key={section.id} as="div" className="border-b border-gray-200 py-6">
                     <h3 className="-mx-2 -my-3 flow-root">
                       <DisclosureButton className="group flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
@@ -261,14 +216,14 @@ export default function Product() {
                         {section.options.map((option, optionIdx) => (
                           <div key={option.value} className="flex items-center">
                             <input
-                              defaultValue={option.value}
+                              value={option.value}
                               defaultChecked={option.checked}
-                              id={`filter-mobile-${section.id}-${optionIdx}`}
-                              name={`${section.id}[]`}
-                              type="checkbox"
+                              id={`filter-desktop-${section.id}-${optionIdx}`}
+                              name={section.id} // Ensure the same name for radio buttons in the same group
+                              type="checkbox" // Change checkbox to radio
                               className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                             />
-                            <label htmlFor={`filter-mobile-${section.id}-${optionIdx}`} className="ml-3 min-w-0 flex-1 text-gray-500">
+                            <label htmlFor={`filter-desktop-${section.id}-${optionIdx}`} className="ml-3 min-w-0 flex-1 text-gray-500">
                               {option.label}
                             </label>
                           </div>
@@ -277,9 +232,8 @@ export default function Product() {
                     </DisclosurePanel>
                   </Disclosure>
                 ))}
-                
-                {singleFilter.map((section) => (
-                  <Disclosure key={section.id} as="div" className="border-b border-gray-200 py-6">
+                 {singleFilter.map((section) => (
+                  <Disclosure key={section.id} as="div" className="border-b border-gray-200  py-6">
                     <h3 className="-my-3 flow-root">
                       <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
                         <span className="font-medium text-gray-900">{section.name}</span>
@@ -294,11 +248,11 @@ export default function Product() {
                         {section.options.map((option, optionIdx) => (
                           <div key={option.value} className="flex items-center">
                             <input
-                              defaultValue={option.value}
+                              value={option.value}
                               defaultChecked={option.checked}
                               id={`filter-${section.id}-${optionIdx}`}
-                              name={`${section.id}[]`}
-                              type="checkbox"
+                              name={section.id} // Ensure the same name for radio buttons in the same group
+                              type="radio" // Change checkbox to radio
                               className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                             />
                             <label htmlFor={`filter-${section.id}-${optionIdx}`} className="ml-3 text-sm text-gray-600">
@@ -312,10 +266,12 @@ export default function Product() {
                 ))}
               </form>
 
-              {/* Product grid */}
-              <div className="lg:col-span-4 w-full">
-                <div className="flex flex-wrap justify-center bg-white py-5">
-                  {mens_kurta.map((item) => <ProductCard key={item.id} product={item} />)}
+              {/* Products */}
+              <div className="lg:col-span-4">
+                <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+                  {mens_kurta.map((item) => (
+                    <ProductCard product={item} />
+                  ))}
                 </div>
               </div>
             </div>
@@ -323,6 +279,5 @@ export default function Product() {
         </main>
       </div>
     </div>
-  );
+  )
 }
-
