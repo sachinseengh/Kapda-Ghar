8e3cd146e406c3bff999f87817c57a9c9d689e0a
Sachin kumar Singh <108042077+sachinseengh@users.noreply.github.com>
2024-12-19 22:32:58 +0545

    action file for order added

--- src/State/store.js
+++ src/State/store.js
@@ -2,10 +2,15 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { thunk } from "redux-thunk"; // Named import instead of default
 import { authReducer } from "./Auth/Reducer";
 import { customerProductReducer } from "./Product/Reducer";
+import { orderReducer } from "./Order/Reducer";
+import { cartReducer } from "./Cart/Reducer";
 
 const rootReducers = combineReducers({
     auth: authReducer,
     product:customerProductReducer,
+    cart:cartReducer,
+    order:orderReducer
+
 });
 
 export const store = legacy_createStore(rootReducers, applyMiddleware(thunk));
